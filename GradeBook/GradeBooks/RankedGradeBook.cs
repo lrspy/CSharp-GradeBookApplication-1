39c4b3fc588137f0b078a744a0eabaa1838b7a26
Elliott deLaunay <edelauna@gmail.com>
2018-08-30 14:29:33 -0400

    added override method for rankedGradeBook

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
 ﻿using GradeBook.Enums;
+using System;
+
 
 namespace GradeBook.GradeBooks
 {
@@ -8,5 +10,37 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (this.Students.Count < 5)
+                throw new System.InvalidOperationException("Not enought Student");
+            else
+            {
+                var listOfAverages = this.Students.AverageGrades.ToArray();
+                var rankedPercentile = Percentile(listOfAverages, averageGrade);
+
+                if (rankedPercentile <= .20)
+                    return 'A';
+                else if (rankedPercentile <= .40)
+                    return 'B';
+                else if (rankedPercentile <= .60)
+                    return 'C';
+                else if (rankedPercentile <= .80)
+                    return 'D';
+                else
+                    return 'F';
+            }
+
+            
+        }
+        public double Percentile(double[] sequence, double averageGrade)
+        {
+            Array.Sort(sequence);
+            int N = sequence.Length;
+            int indexOf = Array.IndexOf(sequence, averageGrade);
+
+            return (indexOf+1)/N; 
+        }
+   
     }
 }
